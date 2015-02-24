d9612e89117163a77c06be0905d300f01ed6493f
vi59 <a13marcosvg@iessanclemente.net>
2015-02-24 01:12:55 +0100

    Actualizacion php y formulario

--- ejemploPost.php
+++ ejemploPost.php
@@ -1,4 +1,25 @@
 <?php
-$nombre = $_POST['nombre'];
-echo $nombre;
+
+
+$nombre = utf8_decode($_POST['nombre']);
+
+
+$apellidos = utf8_decode($_POST['apellidos']);
+
+$email = utf8_decode($_POST['email']);
+
+$telefono = utf8_decode($_POST['telefono']);
+
+$mensajeCliente = utf8_decode($_POST['mensajeCliente']);
+
+echo "<br/>   Los datos recogidos en el formulario que acaba de rellenar son: ";
+
+echo"<br/><br/>Nombre: ". $nombre;
+echo"<br/><br/>Apellidos: ".$apellidos;
+echo"<br/><br/>Email: ".$email;
+echo"<br/><br/>Telefono: ".$telefono;
+echo"<br/><br/>Mensaje enviado por usted:<br/><br/> ".$mensajeCliente;
+
+echo"<br/><br/>Desde <b>Novos Tempos</b> queremos agradecerle su atención. Gracias.</span>";
+
 ?>
